0517a873650a24760d427631e8be6cfb959ccf6a
acahue6 <124596590+acahue6@users.noreply.github.com>
2023-09-26 19:26:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Alright, let's simplify and rephrase the problem set to avoid using functions:
+import student_data
+
+
+
+
+# print(student_data.students)
+students = student_data.students
+print(len(students))
+print(students[0]['Combo,Name'])
+print(students[0]['Email'][0])
+print(students[0]['Email'][1])
+
+#print(students[0]['CPSID'])
+#print(students[1]['Combo,Name'])
+#print(students[1]['Email'][2])
+#print(students[1]['CPSID'])
+#print(students[2]['Combo,Name'])
+
+
+#for loops allow uo to
+#iterate through the data
+#and perform some fuction
+
+for student in students:
+  print(student['Combo,Name'][0])
+  print(student['Email'][0])
+  print(student['Email'][1])
+  print(student['CPSID'])
+  print("_"*25)
